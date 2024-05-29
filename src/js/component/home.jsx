fed33ec797f4646721221f825b1eeacaaa733652
Jaume153 <163338482+Jaume153@users.noreply.github.com>
2024-05-29 08:40:02 +0000

    ahora si que funciona

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "../../styles/index.css"
 
 //include images into your bundle
@@ -9,22 +9,30 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = () => {
     const [selectedColor, setSelectedColor] = useState("red");
 
+    const changeColor = () => {
+        if (selectedColor === "red") {
+            setSelectedColor("yellow");
+        } else if (selectedColor === "yellow") {
+            setSelectedColor("green");
+        } else {
+            setSelectedColor("red");
+        }
+    };
+
     return (
-        <div className="text-center">
+        <div className="text-center">           
             <div className="traffic-light-container">
-                <div 
-                    className={`light red ${selectedColor === "red" ? "glow" : ""}`}
-                    onClick={() => setSelectedColor("red")}
-                ></div>
-                <div 
-                    className={`light yellow ${selectedColor === "yellow" ? "glow" : ""}`}
-                    onClick={() => setSelectedColor("yellow")}
-                ></div>
-                <div 
-                    className={`light green ${selectedColor === "green" ? "glow" : ""}`}
-                    onClick={() => setSelectedColor("green")}
-                ></div>
+                <div className={`light red ${selectedColor === "red" ? "glow" : ""}`}
+                    onClick={() => setSelectedColor("red")}>
+                    </div>
+                <div className={`light yellow ${selectedColor === "yellow" ? "glow" : ""}`}
+                    onClick={() => setSelectedColor("yellow")}>
+                    </div>
+                <div className={`light green ${selectedColor === "green" ? "glow" : ""}`}
+                    onClick={() => setSelectedColor("green")}>
+                    </div>
             </div>
+            <button onClick={changeColor} className="btn btn-success mt-4">Pulsa para cambiar de color</button>
         </div>
     );
 };
